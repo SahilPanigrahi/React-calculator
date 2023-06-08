567c484d63a531671ade7bb2b8578a3be2ed2416
SahilPanigrahi <spanigrahi.gmjc@gmail.com>
2023-06-09 05:11:40 +0530

    react-calculator

---
+++ src/app/components/DigitButton.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "@/app/components/Hero"
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
